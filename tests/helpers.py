c7c3a7c447b7b4604d0680d36f3d4e7eb30f680d
Chris Withers <chris@simplistix.co.uk>
2017-03-31 18:25:55 +0100

    Helper to create a document from a sample name.

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,5 +1,13 @@
 from os.path import dirname, join
 
+from sybil.document import Document
+
 
 def sample_path(name):
     return join(dirname(__file__), 'samples', name)
+
+
+def document_from_sample(name):
+    path = sample_path(name)
+    with open(path) as source:
+        return Document(source.read(), path)
