1599dd7a9c386aea93315120875aa92f14c5beb7
Chris Withers <chris@simplistix.co.uk>
2017-04-07 08:25:20 +0100

    change == None to is None, better style.

--- tests/test_codeblock.py
+++ tests/test_codeblock.py
@@ -9,16 +9,16 @@ def test_basic():
     assert len(regions) == 4
     namespace = document.namespace
     namespace['y'] = namespace['z'] = 0
-    assert regions[0].evaluate(namespace) == None
+    assert regions[0].evaluate(namespace) is None
     assert namespace['y'] == 1
     assert namespace['z'] == 0
     with pytest.raises(Exception) as excinfo:
         regions[1].evaluate(namespace)
     assert str(excinfo.value) == 'boom!'
-    assert regions[2].evaluate(namespace) == None
+    assert regions[2].evaluate(namespace) is None
     assert namespace['y'] == 1
     assert namespace['z'] == 1
-    assert regions[3].evaluate(namespace) == None
+    assert regions[3].evaluate(namespace) is None
     assert namespace['bin'] == b'x'
     assert namespace['uni'] == u'x'
     assert '__builtins__' not in namespace
