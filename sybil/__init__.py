7a478b5821e3e644e510a2da1d647343ec10d078
Chris Withers <chris@simplistix.co.uk>
2017-04-15 19:26:57 +0100

    make the other two parsers currently included imported direct from the package.

--- sybil/__init__.py
+++ sybil/__init__.py
@@ -1,3 +1,5 @@
 from .sybil import Sybil
 from .region import Region
+from .parsers.capture import parse_captures
+from .parsers.codeblock import CodeBlockParser
 from .parsers.doctest import DocTestParser
