4efddb013d7463338be31d1f6e4c4ffd700772c5
Chris Withers <chris@simplistix.co.uk>
2017-06-02 08:37:32 +0100

    Another stab at correctly handling indented codeblocks.

--- sybil/parsers/codeblock.py
+++ sybil/parsers/codeblock.py
@@ -4,11 +4,10 @@ import textwrap
 from sybil import Region
 
 CODEBLOCK_START = re.compile(
-    r'^[ \t]*\.\.\s*(invisible-)?code(-block)?::?\s*python\b'
+    r'^(?P<indent>[ \t]*)\.\.\s*(invisible-)?code(-block)?::?\s*python\b'
     r'(?:\s*\:[\w-]+\:.*\n)*'
     r'(?:\s*\n)*',
     re.MULTILINE)
-CODEBLOCK_END = re.compile(r'(\n\Z|\n(?=\S))')
 
 
 def evaluate_code_block(example):
@@ -32,13 +31,16 @@ class CodeBlockParser(object):
         self.future_imports = future_imports
 
     def __call__(self, document):
-        for start_match, end_match, source in document.find_region_sources(
-            CODEBLOCK_START, CODEBLOCK_END
-        ):
-            source = textwrap.dedent(source)
+        for start_match in re.finditer(CODEBLOCK_START, document.text):
+            source_start = start_match.end()
+            indent = start_match.group('indent')
+            end_pattern = re.compile(r'(\n\Z|\n'+indent+'(?=\S))')
+            end_match = end_pattern.search(document.text, source_start)
+            source_end = end_match.start()
+            source = textwrap.dedent(document.text[source_start:source_end])
             # There must be a nicer way to get code.co_firstlineno
             # to be correct...
-            line_count = document.text.count('\n', 0, start_match.end())
+            line_count = document.text.count('\n', 0, source_start)
             if self.future_imports:
                 line_count -= 1
                 source = 'from __future__ import {}\n{}'.format(
@@ -49,7 +51,7 @@ class CodeBlockParser(object):
             code = compile(source, document.path, 'exec', dont_inherit=True)
             yield Region(
                 start_match.start(),
-                end_match.end(),
+                source_end,
                 code,
                 evaluate_code_block
             )
