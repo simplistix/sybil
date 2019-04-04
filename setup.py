6d3da472676c7d97eec8daa9e57760ce8c426702
Chris Withers <chris@withers.org>
2019-04-04 08:34:39 +0100

    3.7 works

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     packages=find_packages(exclude=['tests', 'functional_tests']),
     zip_safe=False,
