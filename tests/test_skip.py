a7d7392c68618c17e9fe09bc3520be7aa7d14c69
Chris Withers <chris@withers.org>
2019-04-25 08:35:32 +0100

    conditional skip

--- tests/test_skip.py
+++ tests/test_skip.py
@@ -1,8 +1,15 @@
+import sys
+from unittest import SkipTest
+
+import pytest
+
+from sybil.compat import PY3
 from sybil.document import Document
 from sybil.parsers.codeblock import CodeBlockParser
+from sybil.parsers.doctest import DocTestParser
 from sybil.parsers.skip import skip
 
-from .helpers import sample_path
+from .helpers import sample_path, document_from_sample, evaluate_region
 
 
 def test_basic():
@@ -10,3 +17,60 @@ def test_basic():
     for example in document:
         example.evaluate()
     assert document.namespace['run'] == [2, 5]
+
+
+def test_conditional_simple():
+    document = Document.parse(
+        sample_path('skip-conditional-simple.txt'), DocTestParser(), skip
+    )
+    document.namespace['sys'] = sys
+    document.namespace['run'] = []
+    skipped = []
+    for example in document:
+        try:
+            example.evaluate()
+        except SkipTest as e:
+            skipped.append(str(e))
+    assert document.namespace['run'] == [1, 2]
+    # we should always have one and only one skip from this document.
+    if PY3:
+        assert skipped == ['only true on python 2']
+    else:
+        assert skipped == ['only true on python 3']
+
+
+def test_conditional_full():
+    document = Document.parse(
+        sample_path('skip-conditional.txt'), DocTestParser(), skip
+    )
+    document.namespace['result'] = result = []
+    for example in document:
+        try:
+            example.evaluate()
+        except SkipTest as e:
+            result.append('skip:'+str(e))
+    assert result == [
+        'start',
+        'skip:(2 > 1)',
+        'good 1',
+        'skip:foo',
+        'skip:foo',
+        'good 2',
+    ]
+
+
+def test_bad():
+    document = document_from_sample('skip-conditional-bad.txt')
+    regions = list(skip(document))
+    namespace = document.namespace
+
+    with pytest.raises(ValueError) as excinfo:
+        evaluate_region(regions[0], namespace)
+    assert str(excinfo.value) == 'Bad skip action: lolwut'
+
+    with pytest.raises(ValueError) as excinfo:
+        evaluate_region(regions[1], namespace)
+    assert str(excinfo.value) == 'Cannot have condition on end'
+
+    with pytest.raises(SyntaxError):
+        evaluate_region(regions[2], namespace)
