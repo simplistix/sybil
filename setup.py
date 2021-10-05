7c2df8527cafd41fd52d3b753629dd9b845b51ba
Chris Withers <chris@simplistix.co.uk>
2021-10-05 08:28:20 +0100

    Officially support 3.10

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     packages=find_packages(exclude=['tests', 'functional_tests']),
     zip_safe=False,
