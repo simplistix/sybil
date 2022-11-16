1723c6f9967dd76e219ebb7e733b218fb9fc15e3
Chris Withers <chris@simplistix.co.uk>
2022-11-16 07:42:26 +0000

    ReST doctest directive parser.

--- sybil/parsers/rest/__init__.py
+++ sybil/parsers/rest/__init__.py
@@ -1,4 +1,4 @@
 from .capture import CaptureParser
 from .codeblock import CodeBlockParser, PythonCodeBlockParser
-from .doctest import DocTestParser
+from .doctest import DocTestParser, DocTestDirectiveParser
 from .skip import SkipParser
